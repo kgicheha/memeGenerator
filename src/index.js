c378825d345022bc9030b53eaa94feb401a9f510
Chris Hendricks <crshenx@gmail.com>
2022-06-22 13:31:05 -0700

    commented out my work on search

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 //GET DOM ELEMENTS
-const addMeme = document.querySelector('#add-meme')
-const removeMeme = document.querySelector('#remove-form')
-const createMemeForm = document.querySelector('#create-meme')
-const myMemes = document.querySelector('#my-memes')
+const addMeme = document.querySelector("#add-meme");
+const removeMeme = document.querySelector("#remove-form");
+const createMemeForm = document.querySelector("#create-meme");
+const myMemes = document.querySelector("#my-memes");
 
 const memeApiUrl = "https://api.imgflip.com/get_memes";
 const memeOftheDay = document.getElementById("meme-of-the-day-img");
@@ -22,82 +22,76 @@ function renderMeme(memeDataObj) {
 
 generateButton.addEventListener("click", randomizer300);
 
-
 //Event Listener to display form
-addMeme.addEventListener('click', e=> {
-
-    createMemeForm.style.display = "inline-block"
-    addMeme.style.display = "none"
-})
+addMeme.addEventListener("click", (e) => {
+  createMemeForm.style.display = "inline-block";
+  addMeme.style.display = "none";
+});
 
 //Event Listener to remove form
-removeMeme.addEventListener('click', e=> {
-    createMemeForm.style.display = "none"
-    addMeme.style.display = "inline-block"
-})
+removeMeme.addEventListener("click", (e) => {
+  createMemeForm.style.display = "none";
+  addMeme.style.display = "inline-block";
+});
 
 //Event Listener to submit form
 //Add meme to random generator function
-createMemeForm.addEventListener('submit', (e) =>{
-    e.preventDefault();
+createMemeForm.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-    const newMemes = {
-        memeName: e.target.name.value,
-        memeImage: e.target.image.value,
-        likes: 0
-    }
+  const newMemes = {
+    memeName: e.target.name.value,
+    memeImage: e.target.image.value,
+    likes: 0,
+  };
 
-    fetch('http://localhost:3000/memes', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(
-            newMemes
-        )
-    })
-    .then(response => response.json())
-    .then(renderMyMemes)
+  fetch("http://localhost:3000/memes", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(newMemes),
+  })
+    .then((response) => response.json())
+    .then(renderMyMemes);
 
-    createMemeForm.reset()
-})
-
+  createMemeForm.reset();
+});
 
 //fetch data from db.json file
-fetch('http://localhost:3000/memes')
-.then(response => response.json())
-.then(data => {
-    data.forEach(renderMyMemes)
-
-})
+fetch("http://localhost:3000/memes")
+  .then((response) => response.json())
+  .then((data) => {
+    data.forEach(renderMyMemes);
+  });
 
 //display our memes
 function renderMyMemes(object) {
+  // create DOM Element
+  const li = document.createElement("li");
+  const myMemesImage = document.createElement("img");
 
-   // create DOM Element
-    const li = document.createElement('li')
-    const myMemesImage = document.createElement('img')
+  //edit content in DOM element
+  myMemesImage.src = object.memeImage;
 
-    //edit content in DOM element
-    myMemesImage.src = object.memeImage
+  myMemes.className = "meme-pics";
 
-    myMemes.className = "meme-pics"
+  //append
+  li.textContent = myMemesImage;
 
-    //append
-    li.textContent = myMemesImage
-
-    myMemes.append(myMemesImage)
-
-
-
+  myMemes.append(myMemesImage);
+}
 function randomizer300() {
   let memesArr = memeDataVar.data.memes;
   let rando = Math.floor(Math.random() * memesArr.length);
   memeOftheDay.src = memesArr[rando].url;
   // console.log(rando);
-
 }
 
-searchForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-})}
+// searchForm.addEventListener("submit", (e) => {
+//   e.preventDefault();
+//   fetch("https://api.imgflip.com/get_memes")
+//     .then((r) => r.json())
+//     .then((result) => console.log(result));
+//   console.log(e.target.comment.value);
+// });
